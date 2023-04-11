f9253f7d9321dfb71e947e02132d53250bfdcdb1
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-04-12 02:41:07 +0500

    Сообщения с повторением.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            int countOfRepetitions;
+            string userMessage;
+
+            Console.WriteLine("Введите сообщение которое нужно вывести: ");
+            userMessage = Console.ReadLine();
+            Console.WriteLine("Введите сколько раз нужно вывести ваше сообщение: ");
+            countOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            while (countOfRepetitions-- > 0)
+            {
+                Console.WriteLine(userMessage);
+            }
+
             Console.ReadKey();
         }
     }
