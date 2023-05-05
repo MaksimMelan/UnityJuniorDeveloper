d37104e68638d284a7fe365f4d66dcbaaecb1c82
Максим Максимов <maksimum884@gmail.com>
2023-05-05 15:57:51 +0500

    С помощью Random 1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,28 @@
     {
         static void Main(string[] args)
         {
-            int sequenceValue = 5;
-            int progressionDifference = 7;          
-            int finalSequenceValue = 96;
+            Random random = new Random();
+            int minRandomValue = 1;
+            int maxRandomValue = 100;
 
-            for(int i = sequenceValue; i <= finalSequenceValue; i+= progressionDifference)
+            int number = random.Next(minRandomValue, maxRandomValue + 1 );
+            int sum = 0;
+            int divider1 = 3;
+            int divider2 = 5;
+
+            Console.WriteLine($"цифра рандома{number}");
+
+            for (int i = 1; i <= number; i++)
             {
-                Console.WriteLine(i);
+                if (i % divider1 == 0 || i % divider2 == 0)
+                {
+                    sum += i;
+                    Console.WriteLine($"значение {i},  сумма {sum}");
+                }              
             }
+
+            Console.WriteLine($"Число полученное с помощю Рандома: {number}" +
+                $"\nСумма всех чисел кратная {divider1} или {divider2} равна: {sum}");
         }
     }
 }
